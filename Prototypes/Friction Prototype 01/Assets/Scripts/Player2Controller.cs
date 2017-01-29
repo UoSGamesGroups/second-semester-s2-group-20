0a6b183765344a98a674f07dc994614ff04fdf9a
Jake Seaman <S183883@ucs.ac.uk>
2017-01-29 17:33:03 +0000

    Friction Prototype
    Initial upload for the early 'Friction Prototype'

---
+++ Prototypes/Friction Prototype 01/Assets/Scripts/Player2Controller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Controller : MonoBehaviour {
+
+	public Rigidbody2D rb2d;
+
+	void FixedUpdate ()
+	{
+		if (Input.GetKey (KeyCode.KeypadEnter)) {
+			rb2d.constraints = RigidbodyConstraints2D.FreezeAll;
+		}
+	}
+}
---
+++ Prototypes/Friction Prototype 01/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public Rigidbody2D rb2d;
+
+	void FixedUpdate ()
+	{
+		if (Input.GetKey (KeyCode.Space)) {
+			rb2d.constraints = RigidbodyConstraints2D.FreezeAll;
+		}
+	}
+}
