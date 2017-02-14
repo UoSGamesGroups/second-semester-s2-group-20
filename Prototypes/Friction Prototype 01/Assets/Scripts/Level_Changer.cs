4923da1f847b46e1f1bf5b54d483a0728d43a4df
jackfuller1996 <s176802@ucs.ac.uk>
2017-02-14 18:02:20 +0000

    This is the random Level Generator
    This random level Generator, waits 10 seconds from the beginning of the
    level and afterwards switches to another random level.

---
+++ Prototypes/Friction Prototype 01/Assets/Scripts/Level_Changer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level_Changer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Change ());
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	IEnumerator Change()
+	{
+		yield return new WaitForSeconds (6);
+		Application.LoadLevel (Random.Range (0, 5));
+	}
+}
