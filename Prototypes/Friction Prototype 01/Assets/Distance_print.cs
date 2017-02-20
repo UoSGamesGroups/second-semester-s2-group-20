682f3a2a08e21af4655742ac5b8f07fec058c988
jackfuller1996 <s176802@ucs.ac.uk>
2017-02-20 16:04:33 +0000

    Measures how far away from the end
    This update just shows you how far you are away from the end goal

---
+++ Prototypes/Friction Prototype 01/Assets/Distance_print.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+using UnityEngine.UI;
+
+public class Distance_print : MonoBehaviour{
+	public Transform player; 
+	public Rigidbody2D rb1; 
+	public Rigidbody2D rb2;
+	float Player1X; 
+	float Player2X;
+	public string information1; 
+	public string information2;
+
+	public Text Text;  
+
+	public Text Textp2;
+
+	public float distance;
+
+
+	void Update()
+	{
+		Player1X = rb1.position.x; 
+		Player2X = rb2.position.x;
+		information1 = Player1X.ToString ("F2"); 
+		information2 = Player2X.ToString ("F2");
+		Text.text = "Player 1 " + information1;
+		Textp2.text = "Player 2 " + information2;
+	}
+}
