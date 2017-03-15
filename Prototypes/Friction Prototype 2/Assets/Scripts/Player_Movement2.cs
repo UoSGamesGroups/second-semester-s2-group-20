afe0bad66289b2897bdd2472e040859bce1e030a
jackfuller1996 <s176802@ucs.ac.uk>
2017-03-15 07:22:08 +0000

    Players now have control over one anothers time
    I have put in the code where if player 2 presses E and player 1 presses
    enter than the players speeds will change and after 2 seconds their
    speed goes back to normal.

--- Prototypes/Friction Prototype 2/Assets/Scripts/Player_Movement2.cs
+++ Prototypes/Friction Prototype 2/Assets/Scripts/Player_Movement2.cs
@@ -2,26 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bounce_Pad : MonoBehaviour {
+public class Player_Movement2: MonoBehaviour {
 
-	public KeyCode Up, Down;
+	public KeyCode A, E, D;
+	public float speed;
+	public GameObject P1;
+	public Rigidbody2D P1speed;
+	private Vector2 velocity;
+	private Vector3 defaultScale;
 
 	// Use this for initialization
 	void Start () {
+		defaultScale = GetComponent<Transform> ().localScale;
+		P1speed = P1.GetComponent<Rigidbody2D>();
+
 	}
 
 	// Update is called once per frame
 	void Update () {
 		UserInput ();
+		velocity *= Time.deltaTime;
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (velocity.x, GetComponent<Rigidbody2D> ().velocity.y + velocity.y);
+		velocity = Vector2.zero;
 	}
 
 	private void UserInput()
 	{
-		if (Input.GetKey (Up)) {
-			transform.Rotate (0, 0, 100 * Time.deltaTime);
+		if (Input.GetKey (A)) {
+			velocity.x = -speed;
+			GetComponent<Transform> ().localScale = new Vector3 (-defaultScale.x, defaultScale.y, defaultScale.z);
+		} else if (Input.GetKey (D)) {
+			velocity.x = speed;
+			GetComponent<Transform> ().localScale = new Vector3 (defaultScale.x, defaultScale.y, defaultScale.z);
+		} else {
+			velocity.x = 0;
 		}
-		else if (Input.GetKey (Down)) {
-			transform.Rotate (0, 0, -100 * Time.deltaTime);
+
+		if (Input.GetKey (E)) {
+			P1speed.drag = 175;
+			StartCoroutine (Fast ());
 		}
+	}
+
+	IEnumerator Fast()
+	{
+		yield return new WaitForSeconds (2);
+		P1speed.drag = 0;
 	}
 }
