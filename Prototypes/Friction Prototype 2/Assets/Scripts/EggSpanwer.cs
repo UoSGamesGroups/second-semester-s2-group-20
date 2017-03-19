116620be1201328bcf7bf605ec9017f6af479432
jackfuller1996 <s176802@ucs.ac.uk>
2017-03-19 19:26:31 +0000

    Egg Spawner
    This update is where 3 different eggs spawn at the top of the sceen and
    wait a few seconds before being destroyed

---
+++ Prototypes/Friction Prototype 2/Assets/Scripts/EggSpanwer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggSpanwer : MonoBehaviour {
+	public Transform[] spawnLocations;
+	public GameObject[] SpawnPrefab;
+	public GameObject[] SpawnClone;
+	public float spawnTime = 3f;
+
+	void Start(){
+		InvokeRepeating ("spawneggs", spawnTime, spawnTime);
+	}
+
+	void spawneggs()
+	{
+		SpawnClone [0] = Instantiate (SpawnPrefab[0], spawnLocations[0].transform.position,Quaternion.Euler(0,0,0)) as GameObject;
+		SpawnClone [1] = Instantiate (SpawnPrefab[1], spawnLocations[1].transform.position,Quaternion.Euler(0,0,0)) as GameObject;
+		SpawnClone [2] = Instantiate (SpawnPrefab[2], spawnLocations[2].transform.position,Quaternion.Euler(0,0,0)) as GameObject;
+
+	}
+}
---
+++ Prototypes/Friction Prototype 2/Assets/Scripts/egg_destroy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class egg_destroy : MonoBehaviour {
+
+	public float timer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timer += 1.0f * Time.deltaTime;
+		if (timer >= 4) {
+			GameObject.Destroy (gameObject);
+		}
+		
+	}
+}
