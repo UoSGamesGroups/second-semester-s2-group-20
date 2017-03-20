5624b912cdad6d21a98ef131fe011bac2a63d05e
jackfuller1996 <s176802@ucs.ac.uk>
2017-03-20 20:19:04 +0000

    Try to make score system work

---
+++ Prototypes/Friction Prototype 2/Assets/Scripts/Score_System.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Score_System : MonoBehaviour {
+
+	private int score;
+	public Text scoreText;
+	public BoxCollider2D bc;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		UpdateScore ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.tag == "Egg") {
+			UpdateScore ();
+		}
+	}
+
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore ();
+	}
+
+	void UpdateScore ()
+	{
+		scoreText.text = "Score: " + score;
+	}
+			
+}
